fdf8ca7aa2131b336d9e25b73e39cd1ae48d18c6
cc-brayan-isaza <brayan.isaza@finoservices.com>
2024-05-21 18:09:45 -0500

    Starting with the main screen

--- Modern-UI/app/(tabs)/_layout.tsx
+++ Modern-UI/app/(tabs)/_layout.tsx
@@ -1,9 +1,110 @@
-import { Text, View } from 'react-native'
+import { Image, Text, View, ImageSourcePropType } from 'react-native'
+import { Tabs, Redirect } from 'expo-router'
+import { icons } from '@/constants'
+
+interface TabIconProps {
+  icon: string
+  color: string
+  name: string
+  focused: boolean
+}
+
+const TabIcon = ({ icon, color, name, focused }: TabIconProps) => {
+  return (
+    <View className=' items-center justify-center gap-2'>
+      <Image
+        source={{ uri: icon }}
+        resizeMode='contain'
+        tintColor={color}
+        className='w-6 h-6'
+      />
+      <Text
+        className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}
+        style={{ color }}
+      >
+        {name}
+      </Text>
+    </View>
+  )
+}
 
 export const TabsLayout = () => {
   return (
-    <View>
-      <Text>Tab</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#ffa001',
+        tabBarInactiveTintColor: '#cdcde0',
+        tabBarStyle: {
+          backgroundColor: '#161622',
+          borderTopWidth: 1,
+          borderTopColor: '#232533'
+        }
+      }}
+    >
+      <Tabs.Screen
+        name='home'
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              name='Home'
+              focused={focused}
+            />
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name='Bookmark'
+        options={{
+          title: 'Bookmark',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.bookmark}
+              color={color}
+              name='Bookmark'
+              focused={focused}
+            />
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name='Create'
+        options={{
+          title: 'Create',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.plus}
+              color={color}
+              name='Create'
+              focused={focused}
+            />
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name='Profile'
+        options={{
+          title: 'Profile',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.profile}
+              color={color}
+              name='Profile'
+              focused={focused}
+            />
+          )
+        }}
+      />
+    </Tabs>
   )
 }
--- Modern-UI/app/index.tsx
+++ Modern-UI/app/index.tsx
@@ -1,18 +1,43 @@
-import { View, Text } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
-import { Link } from '@react-navigation/native'
+import { images } from '@/constants'
+import { Image, ScrollView, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className='text-3xl font-pblack'>Moder ui</Text>
-      <StatusBar style='auto' />
-      <Link
-        to='/home'
-        style={{ color: 'purple' }}
-      >
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className='w-full items-center h-full px-4'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[130px] h-[84px]'
+          />
+
+          <Image
+            source={images.cards}
+            resizeMode='contain'
+            className='max-w-[380px] w-full h-[300px]'
+          />
+
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center'>
+              Discover Endless Possibilities with{' '}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              resizeMode='contain'
+              className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+            />
+          </View>
+
+          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
