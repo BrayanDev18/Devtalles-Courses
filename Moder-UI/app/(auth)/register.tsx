05fc2f8d5863b9e6c445be396e6fb59aed5e9875
cc-brayan-isaza <brayan.isaza@finoservices.com>
2024-05-23 12:24:01 -0500

    Project configuration, structure and beginning of the main screen

---
+++ Moder-UI/app/(auth)/register.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+
+export default function Register() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Text className='text-yellow-500'>Welcome to register</Text>
+    </View>
+  )
+}
---
+++ Moder-UI/app/index.tsx
@@ -0,0 +1,46 @@
+import { images } from '@/constants'
+import { Image, ScrollView, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+
+export default function Index() {
+  return (
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className='w-full items-center h-full px-4'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[130px] h-[84px]'
+          />
+
+          <Image
+            source={images.cards}
+            resizeMode='contain'
+            className='max-w-[380px] w-full h-[300px]'
+          />
+
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center'>
+              Discover Endless Possibilities with{' '}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              resizeMode='contain'
+              className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+            />
+          </View>
+
+          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+
+      <StatusBar style='light' />
+    </SafeAreaView>
+  )
+}
