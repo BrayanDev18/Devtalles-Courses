89a9af56e37d33e4d1ced534419e2cba2584ffcb
cc-brayan-isaza <brayan.isaza@finoservices.com>
2024-06-14 18:03:13 -0500

    Se crea inicio del projecto de e-commerce

---
+++ Projects/e-commerce/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen bg-black text-white">
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
